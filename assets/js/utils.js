f3248f1a05d36363567fa8ae57ddbd2ffc7df515
Minoru OSAWA <osawa.minoru@gmail.com>
2026-07-12 23:36:22 +0900

    no toggle menu on mobile

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,29 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const siteNav = document.querySelector(".site-nav");
-  const navToggle = siteNav ? siteNav.querySelector(".nav-toggle") : null;
-  const navMenu = siteNav ? siteNav.querySelector(".site-nav-collapse") : null;
-
-  if (siteNav && navToggle && navMenu) {
-    const setNavExpanded = (expanded) => {
-      siteNav.classList.toggle("is-open", expanded);
-      navToggle.setAttribute("aria-expanded", String(expanded));
-    };
-
-    navToggle.addEventListener("click", () => {
-      setNavExpanded(navToggle.getAttribute("aria-expanded") !== "true");
-    });
-
-    navMenu.querySelectorAll("a").forEach(link => {
-      link.addEventListener("click", () => setNavExpanded(false));
-    });
-
-    document.addEventListener("keydown", event => {
-      if (event.key === "Escape") {
-        setNavExpanded(false);
-      }
-    });
-  }
-
   // 外部リンク / PDF を _blank に
   document.querySelectorAll('a[href]').forEach(link => {
     const href = link.getAttribute("href");
